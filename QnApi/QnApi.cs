ca3474f56334f5b3e41c5b8c2aac9dc77eee5f27
Unknown <stephannielsen@gmail.com>
2017-05-30 00:10:15 +0200

    Added class library for QNAP api access.
    Added sample config file with dummy values.

---
+++ QnApi/QnApi.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace QnApi
+{
+  public class QnApi
+  {
+  }
+}
---
+++ QnApi/QnApiConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QnApi
+{
+  public class QnApiConfig
+  {
+    public const string Protocol = "https";
+    public const string AppName = "downloadstation";
+    public const string ApiVersion = "V4";
+    public const string Url = "<your-ip>";
+    public const string Username = "<your-username>";
+    public const string Password = "<your-password>";
+  }
+}
