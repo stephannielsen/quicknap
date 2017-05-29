e0aa312a3ba1f321e8600f28db0db09ea9d12462
Unknown <stephannielsen@gmail.com>
2017-05-30 01:01:41 +0200

    Work in progress on the QNAP api.

--- QnApi/QnApi.cs
+++ QnApi/QnApi.cs
@@ -1,8 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static QnApi.QnApiConfig;
 
 namespace QnApi
 {
   public class QnApi
   {
+    private readonly string _host;
+    private readonly string _username;
+    private readonly string _password;
+
+    private string _sessionId = string.Empty;
+
+    public string BasePath => $"{Protocol}://{_host}/{AppName}/{ApiVersion}";
+
+    public QnApi(string host, string username, string password)
+    {
+      _host = host;
+      _username = username;
+      _password = password;
+    }
+
+    private async Task<string> GetSessionId()
+    {
+      if (string.IsNullOrEmpty(_sessionId))
+        await LoadSessionId();
+      return _sessionId;
+    }
+
+    private async Task LoadSessionId()
+    {
+      var payload = new FormUrlEncodedContent(new[]
+      {
+        new KeyValuePair<string, string>("user", _username),
+        new KeyValuePair<string, string>("pass", Convert.ToBase64String(Encoding.UTF8.GetBytes(_password)))
+      });
+      var client = new HttpClient();
+      client.BaseAddress = new Uri(BasePath);
+      var response = await client.PostAsync(GetUrlForPath(MethodGroup.Misc, Method.Login), payload);
+      response.EnsureSuccessStatusCode();
+      var json = await response.Content.ReadAsStringAsync()
+
+    }
+
+    private string GetUrlForPath(MethodGroup group, Method method)
+    {
+      return $"{BasePath}/{group.ToString()}/{method.ToString()}";
+    }
   }
 }
--- QnApi/QnApiConfig.cs
+++ QnApi/QnApiConfig.cs
@@ -9,8 +9,61 @@ namespace QnApi
     public const string Protocol = "https";
     public const string AppName = "downloadstation";
     public const string ApiVersion = "V4";
-    public const string Url = "<your-ip>";
-    public const string Username = "<your-username>";
-    public const string Password = "<your-password>";
+
+    public static Dictionary<MethodGroup, IEnumerable<Method>> ApiMethods => new Dictionary<MethodGroup, IEnumerable<Method>>
+    {
+      { MethodGroup.Misc, new List<Method> { Method.Dir, Method.Env, Method.Login, Method.Logout, Method.Socks_5 } },
+      { MethodGroup.Task, new List<Method> { Method.Status, Method.Query, Method.Detail, Method.Add_Url, Method.Add_Torrent, Method.Start, Method.Stop, Method.Pause, Method.Remove, Method.Priority, Method.Get_File, Method.Set_File } },
+      { MethodGroup.Rss, new List<Method> { Method.Add, Method.Query, Method.Update, Method.Remove, Method.Query_Feed, Method.Update_Feed, Method.Add_Job, Method.Query_Job, Method.Update_Job, Method.Remove_Job } },
+      { MethodGroup.Config, new List<Method> { Method.Get, Method.Set } },
+      { MethodGroup.Account, new List<Method> { Method.Add, Method.Query, Method.Update, Method.Remove } },
+      { MethodGroup.Addon, new List<Method> { Method.Query, Method.Enable, Method.Verify, Method.Install, Method.Uninstall, Method.Search } }
+    };
+
+    public enum MethodGroup
+    {
+      Misc,
+      Task,
+      Rss,
+      Config,
+      Account,
+      Addon
+    }
+
+    public enum Method
+    {
+      Dir,
+      Env,
+      Login,
+      Logout,
+      Socks_5,
+      Status,
+      Query,
+      Detail,
+      Add_Url,
+      Add_Torrent,
+      Start,
+      Stop,
+      Pause,
+      Remove,
+      Priority,
+      Get_File,
+      Set_File,
+      Add,
+      Update,
+      Query_Feed,
+      Update_Feed,
+      Add_Job,
+      Query_Job,
+      Update_Job,
+      Remove_Job,
+      Get,
+      Set,
+      Enable,
+      Verify,
+      Install,
+      Uninstall,
+      Search
+    }
   }
 }
